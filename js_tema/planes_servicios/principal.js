69fbe96cddc091d2018bc03557373842d90e8022
arithgrey <arithgrey@gmail.com>
2019-01-14 02:35:43 +0100

    [chore]

--- js_tema/planes_servicios/principal.js
+++ js_tema/planes_servicios/principal.js
@@ -1330,8 +1330,6 @@ var set_cantidad_en_stock = function(){
 
     var stock       = get_parameter(".stock");
     var id_servicio = get_parameter(".id_servicio");
-    alert(id_servicio);
-
     var data_send   = $.param({"stock":  stock , "id_servicio" : id_servicio });
     var url         = $(".form_stock").attr("action");
     request_enid("PUT", data_send, url, function () {
