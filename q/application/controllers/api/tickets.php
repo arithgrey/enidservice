714822b9109c4f487b9b25170de7367cf89c8422
arihgrey@gmail.com <arihgrey@gmail.com>
2019-07-18 09:27:42 +0200

    [FIX] tickets

--- q/application/controllers/api/tickets.php
+++ q/application/controllers/api/tickets.php
@@ -21,9 +21,14 @@ class Tickets extends REST_Controller
         $response = false;
         if (fx($param, "id_usuario")) {
 
-
-            $id_usuario = $param["id_usuario"];
-            $response = $this->tickets_model->get(["id_ticket", "asunto"], ["id_usuario" => $id_usuario , "status" => 1 ] , 10);
+            $response = $this->tickets_model->get(
+                ["id_ticket", "asunto"],
+                [
+                    "id_usuario" => $param["id_usuario"],
+                    "status" => 1
+                ],
+                10
+            );
 
         }
         $this->response($response);
@@ -48,13 +53,12 @@ class Tickets extends REST_Controller
         $response = false;
         if (fx($param, "id_ticket")) {
 
-
-            $data =  [
+            $data = [
                 "info_ticket" => $this->tickets_model->get_info_ticket($param),
                 "info_tareas" => $this->get_tareas_ticket($param),
                 "info_num_tareas" => $this->get_tareas_ticket_num($param)
-
             ];
+
             $this->response(format_tareas($data));
 
         }
@@ -119,11 +123,7 @@ class Tickets extends REST_Controller
     function form_GET()
     {
 
-        $param = $this->get();
-        $departamentos = $this->get_departamentos($param);
-        $response = get_format_tickets($departamentos);
-        $this->response($response);
-
+        $this->response(get_format_tickets($this->get_departamentos($this->get())));
     }
 
     function cancelar_form_GET()
@@ -135,13 +135,10 @@ class Tickets extends REST_Controller
         if (fx($param, "id_recibo,modalidad")) {
             $modalidad = $param["modalidad"];
 
-            if ($modalidad == 1) {
+            $recibo = ($modalidad == 1) ?
+                $this->get_recibo_por_enviar($param) :
+                $this->get_recibo_por_pagar($param);
 
-                $recibo = $this->get_recibo_por_enviar($param);
-
-            } else {
-                $recibo = $this->get_recibo_por_pagar($param);
-            }
 
             $response = form_cancelar_compra($recibo, $modalidad);
         }
@@ -177,11 +174,9 @@ class Tickets extends REST_Controller
 
                 /*Si la cuenta pertenece hay que realizar la cancelación del la órden de pago*/
                 $data_complete["registro"] = $this->cancelar_orden_compra($param);
-
-                $usuario_notificado = $data["recibo"]["id_usuario"];
                 $prm["id_recibo"] = $param["id_recibo"];
                 $prm["usuario_notificado"] =
-                $data_complete["info_cliente"] = $this->app->usuario($usuario_notificado);
+                $data_complete["info_cliente"] = $this->app->usuario($data["recibo"]["id_usuario"]);
                 $data_complete["info_email"] = $this->notifica_venta_cancelada_a_cliente($prm);
 
 
@@ -196,9 +191,12 @@ class Tickets extends REST_Controller
 
         $this->cancela_orden_compra($param);
         $id_servicio = $this->get_servicio_ppfp($param["id_recibo"]);
-        $response["id_servicio"] = $id_servicio;
-        $response["gamificacion"] = $this->gamificacion_negativa($id_servicio, $param["id_usuario"]);
-        return $response;
+
+        return [
+            "id_servicio" => $id_servicio,
+            "gamificacion" => $this->gamificacion_negativa($id_servicio, $param["id_usuario"])
+        ];
+
     }
 
     function movimientos_usuario_GET()
@@ -246,7 +244,7 @@ class Tickets extends REST_Controller
                 case 3:
 
                     $tickets = $this->tickets_model->get_tickets($param);
-                    $response =  format_tablero($tickets);
+                    $response = format_tablero($tickets);
 
                     break;
 
@@ -266,8 +264,7 @@ class Tickets extends REST_Controller
     {
 
         $param = $this->get();
-        $response = get_form_respuesta($param["tarea"]);
-        $this->response($response);
+        $this->response(get_form_respuesta($param["tarea"]));
     }
 
     function estado_PUT()
@@ -284,15 +281,13 @@ class Tickets extends REST_Controller
     private function enviar($q)
     {
 
-        $api = "areacliente/enviar/";
-        return $this->app->api($api, $q, "json", "POST");
+        return $this->app->api("areacliente/enviar/", $q, "json", "POST");
     }
 
     private function get_mensaje_notificacion($q)
     {
 
-        $api = "cron/ticket_soporte/";
-        return $this->app->api($api, $q, "html", "GET");
+        return $this->app->api("cron/ticket_soporte/", $q, "html", "GET");
 
     }
 
@@ -300,7 +295,7 @@ class Tickets extends REST_Controller
     {
 
 
-        return  $this->app->api("tarea/ticket/format/json/", $q, "json", "GET", 1);
+        return $this->app->api("tarea/ticket/format/json/", $q, "json", "GET", 1);
 
     }
 
@@ -314,56 +309,54 @@ class Tickets extends REST_Controller
     private function cancela_orden_compra($q)
     {
 
-        $api = "recibo/cancelar";
-        return $this->app->api($api, $q, "json", "PUT");
+        return $this->app->api("recibo/cancelar", $q, "json", "PUT");
     }
 
     private function get_servicio_ppfp($id_recibo)
     {
 
-        $q["id_recibo"] = $id_recibo;
-        $api = "recibo/servicio_ppfp/format/json/";
-        return $this->app->api($api, $q);
+        return $this->app->api("recibo/servicio_ppfp/format/json/", ["id_recibo" => $id_recibo ]);
     }
 
     private function gamificacion_negativa($id_servicio, $id_usuario)
     {
 
-        $q["id_servicio"] = $id_servicio;
-        $q["type"] = 2;
-        $q["id"] = $id_servicio;
-        $q["id_usuario"] = $id_usuario;
-        $api = "servicio/add_gamification_servicio";
-        return $this->app->api($api, $q, "json", "PUT");
+
+
+        $q =  [
+            "id_servicio" => $id_servicio,
+            "type" => 2,
+            "id" => $id_servicio,
+            "id_usuario" => $id_usuario,
+
+        ];
+
+        return $this->app->api("servicio/add_gamification_servicio", $q, "json", "PUT");
     }
 
     private function get_recibo_por_pagar($q)
     {
 
-        $api = "recibo/recibo_por_pagar_usuario/format/json/";
-        return $this->app->api($api, $q);
+        return $this->app->api("recibo/recibo_por_pagar_usuario/format/json/", $q);
     }
 
     private function get_recibo_por_enviar($q)
     {
 
-        $api = "recibo/recibo_por_enviar_usuario/format/json/";
-        return $this->app->api($api, $q);
+        return $this->app->api("recibo/recibo_por_enviar_usuario/format/json/", $q);
     }
 
     private function notifica_venta_cancelada_a_cliente($q)
     {
 
-        $api = "cobranza/cancelacion_venta/format/json/";
-        return $this->app->api($api, $q);
+        return $this->app->api("cobranza/cancelacion_venta/format/json/", $q);
 
     }
 
     private function get_departamentos($q)
     {
 
-        $api = "departamento/index/format/json/";
-        return $this->app->api($api, $q);
+        return $this->app->api("departamento/index/format/json/", $q);
 
     }
 
