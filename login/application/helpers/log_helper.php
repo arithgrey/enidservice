75bf1ba459eca16e29003a79b0ed3e95821b2b60
Jonathan.medrano <jonathan.medrano@benandfrank.com>
2019-10-30 19:27:03 -0600

    [chore]

--- login/application/helpers/log_helper.php
+++ login/application/helpers/log_helper.php
@@ -10,11 +10,11 @@ if (!function_exists('invierte_date_time')) {
         $r[] = recuperacion();
         $r[] = frm_acceso($action);
         $r[] = hiddens(
-                [
-                        "class" => "action",
-                        "value" => $action,
-                ],
-                1
+            [
+                "class" => "action",
+                "value" => $action,
+            ],
+            1
         );
 
         return d(d(append($r), 4, 1), "col-lg-12");
@@ -28,20 +28,20 @@ if (!function_exists('invierte_date_time')) {
         if ($action === "registro") {
 
             $r[] = contaiter(
-                    d("Tu usuario fué registrado, accede ahora!",
-                            "inf_usuario_registrado strong mt-5 bg-light p-2 text-right")
-                    ,
-                    1
+                d("Tu usuario fué registrado, accede ahora!",
+                    "inf_usuario_registrado strong mt-5 bg-light p-2 text-right")
+                ,
+                1
             );
         }
         $r[] = place("place_acceso_sistema mt-5 mb-5", 1);
         $x[] = a_enid("¿ERES NUEVO?   REGISTRATE!", ["class" => 'strong black registrar_cuenta f16 strong']);
         $x[] = a_enid(
-                "¿OLVIDASTE TU CONTRASEÑA?",
-                [
-                        "id" => "olvide-pass",
-                        "class" => "recupara-pass  olvide_pass top_30 underline",
-                ]
+            "¿OLVIDASTE TU CONTRASEÑA?",
+            [
+                "id" => "olvide-pass",
+                "class" => "recupara-pass  olvide_pass top_30 underline",
+            ]
         );
         $r[] = append($x);
 
@@ -52,15 +52,18 @@ if (!function_exists('invierte_date_time')) {
     function img_default()
     {
         return contaiter(
-                d(
-                        a_enid(
-                                img_enid(
-                                        ["class" => "w-75"]),
+            d(
+                a_enid(
+                    img_enid(
+                        [
+                            "class" => "w-75"
+                        ]
+                    ),
 
-                                path_enid("home")
+                    path_enid("home")
 
-                        ), "text-center"
-                )
+                ), "text-center"
+            )
         );
     }
 
@@ -69,25 +72,25 @@ if (!function_exists('invierte_date_time')) {
 
         $r[] = img_default();
         $r[] = h('RECUPERA TUS DATOS DE ACCESO', 3);
-        $r[] = "<form class='form-pass' id='form-pass' action='".url_recuperacion_password()."'>";
+        $r[] = "<form class='form-pass' id='form-pass' action='" . url_recuperacion_password() . "'>";
         $r[] = input(
-                [
-                        "type" => "email",
-                        "id" => "email_recuperacion",
-                        "name" => 'mail',
-                        "placeholder" => "Email",
-                        "class" => " top_10",
-                        "required" => true,
-                ]
+            [
+                "type" => "email",
+                "id" => "email_recuperacion",
+                "name" => 'mail',
+                "placeholder" => "Email",
+                "class" => " top_10",
+                "required" => true,
+            ]
         );
         $r[] = d("Ingresa tu correo electrónico y tu contraseña será enviada", 'mt-4');
         $r[] = btn("Enviar",
-                [
-                        "class" => "btn_nnuevo recupera_password  a_enid_blue top_20",
-                ]
+            [
+                "class" => "btn_nnuevo recupera_password  a_enid_blue top_20",
+            ]
         );
         $extra = add_text(
-                place("place_recuperacion_pw"), place("recuperacion_pw"));
+            place("place_recuperacion_pw"), place("recuperacion_pw"));
         $r[] = form_close($extra);
         $r[] = ya_registro();
         $response = d(append($r), "contenedor_recuperacion_password display_none");
@@ -100,12 +103,12 @@ if (!function_exists('invierte_date_time')) {
     {
 
         return d(
-                a_enid("¿YA ESTÁS REGISTRADO? ACCEDE!",
-                        [
-                                'class' => 'top_50 black bottom_100 strong f16 btn_acceder_cuenta_enid',
-                        ]
-                ),
-                "text-center"
+            a_enid("¿YA ESTÁS REGISTRADO? ACCEDE!",
+                [
+                    'class' => 'top_50 black bottom_100 strong f16 btn_acceder_cuenta_enid',
+                ]
+            ),
+            "text-center"
         );
     }
 
@@ -127,46 +130,46 @@ if (!function_exists('invierte_date_time')) {
         $r[] = form_open("", ["class" => "form-miembro-enid-service", "id" => "form-miembro-enid-service"]);
 
         $r[] = input_frm(
-                "mt-5", "TU NOMBRE",
-                [
-                        "name" => "nombre",
-                        "placeholder" => "TU NOMBRE",
-                        "class" => "nombre_persona",
-                        "type" => "text",
-                        "required" => true,
-                        "id" => "registro_nombre",
-                ]
+            "mt-5", "TU NOMBRE",
+            [
+                "name" => "nombre",
+                "placeholder" => "TU NOMBRE",
+                "class" => "nombre_persona",
+                "type" => "text",
+                "required" => true,
+                "id" => "registro_nombre",
+            ]
         );
         $r[] = place("place_nombre_info");
         $r[] = input_frm("mt-5", "CORREO ELECTRÓNICO",
-                [
-                        "name" => "email",
-                        "placeholder" => "CORREO",
-                        "class" => "email registro_email ",
-                        "type" => "email",
-                        "required" => true,
-                        "onkeypress" => "minusculas(this);",
-                        "id" => "registro_email",
-                ]
+            [
+                "name" => "email",
+                "placeholder" => "CORREO",
+                "class" => "email registro_email ",
+                "type" => "email",
+                "required" => true,
+                "onkeypress" => "minusculas(this);",
+                "id" => "registro_email",
+            ]
         );
         $r[] = place("place_correo_incorrecto");
         $r[] = input_frm(
-                "mt-5", "UNA CONTRASEÑA",
-                [
-                        "id" => "password",
-                        "placeholder" => "UNA CONTRASEÑA ",
-                        "class" => "password registro_pw",
-                        "type" => "password",
-                        "required" => true,
-                        "id" => "registro_pw",
+            "mt-5", "UNA CONTRASEÑA",
+            [
+                "id" => "password",
+                "placeholder" => "UNA CONTRASEÑA ",
+                "class" => "password registro_pw",
+                "type" => "password",
+                "required" => true,
+                "id" => "registro_pw",
 
-                ]
+            ]
         );
         $r[] = place("place_password_afiliado");
         $r[] = btn('Registrar',
-                [
-                        "class" => "top_20",
-                ]
+            [
+                "class" => "top_20",
+            ]
         );
         $r[] = form_close(place("place_registro_miembro"));
 
@@ -178,49 +181,49 @@ if (!function_exists('invierte_date_time')) {
     {
 
         $attr = add_attributes(
-                [
-                        "class" => "form_sesion_enid",
-                        "id" => "in",
-                        "method" => "POST",
-                        "action" => base_url('index.php/api/sess/start/format/json'),
-                ]
+            [
+                "class" => "form_sesion_enid",
+                "id" => "in",
+                "method" => "POST",
+                "action" => base_url('index.php/api/sess/start/format/json'),
+            ]
         );
-        $r[] = "<form ".$attr.">";
+        $r[] = "<form " . $attr . ">";
         $r[] = input(
-                [
-                        "name" => get_random(),
-                        "value" => get_random(),
-                        "type" => "hidden",
-                ]
+            [
+                "name" => get_random(),
+                "value" => get_random(),
+                "type" => "hidden",
+            ]
         );
         $r[] = input(
-                [
-                        "type" => 'hidden',
-                        "name" => 'secret',
-                        "id" => "secret",
-                ]
+            [
+                "type" => 'hidden',
+                "name" => 'secret',
+                "id" => "secret",
+            ]
         );
         $r[] = input_frm(
-                " mt-4", "CORREO ELECTRÓNICO",
-                [
+            " mt-4", "CORREO ELECTRÓNICO",
+            [
 
-                        "type" => "email",
-                        "name" => 'mail',
-                        "id" => "mail_acceso",
-                        "onkeypress" => "minusculas(this);",
-                        "placeholder" => "ej. jonathan@gmail.com",
-                ]
+                "type" => "email",
+                "name" => 'mail',
+                "id" => "mail_acceso",
+                "onkeypress" => "minusculas(this);",
+                "placeholder" => "ej. jonathan@gmail.com",
+            ]
         );
         $r[] = input_frm(
-                "mt-5", "PASSWORD",
-                [
-                        "type" => "password",
-                        "placeholder" => "Tu contraseña",
-                        "name" => 'pw',
-                        "id" => "pw",
+            "mt-5", "PASSWORD",
+            [
+                "type" => "password",
+                "placeholder" => "Tu contraseña",
+                "name" => 'pw',
+                "id" => "pw",
 
-                        "placeholder" => "****",
-                ]
+                "placeholder" => "****",
+            ]
         );
         $r[] = btn("INICIAR", ["class" => "mt-5"]);
         $r[] = form_close();
--- orden_pago_oxxo/application/helpers/saldos_helper.php
+++ orden_pago_oxxo/application/helpers/saldos_helper.php
@@ -1,194 +1,192 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 if (!function_exists('invierte_date_time')) {
-    
-        function format_orden_compra($usuario, $pago, $numero_cuenta)
-        {
+
+    function format_orden_compra($usuario, $pago, $numero_cuenta)
+    {
+
+        $beneficiario = _text(
+            pr($usuario, "nombre"),
+            " ",
+            pr($usuario, "apellido_paterno"),
+            " ",
+            pr($usuario, "apellido_materno")
+        );
+
+        $folio = prm_def($pago, "q2", "");
+
+        $monto = $pago["q"];
+        $concepto = "Saldo a cuenta Enid Service";
+
+        $r[] = d(form_saldos($beneficiario, $folio, $monto, $concepto, $numero_cuenta));
+
+        $contendor_oxoo = btw(
+            img(
+                [
+                    'src' => "http://enidservices.com/inicio/img_tema/portafolio/oxxo-logo.png",
+                    'style' => "width:100px!important;"
+                ]
+            ),
+
+            d("ORDEN DE PAGO EN SUCURSALES OXXO"),
+            "display_flex_enid"
+        );
 
 
-            $beneficiario = append([
-                pr($usuario, "nombre"),
-                " ",
-                pr($usuario, "apellido_paterno"),
-                " ",
-                pr($usuario, "apellido_materno")
-            ]);
+        $r[] = d($contendor_oxoo, ["style" => "background: #02223e;color: white;"]);
+        $str = _text(
+            $concepto,
+            "Beneficiario",
+            $beneficiario,
+            "Folio #",
+            $folio
+
+        );
+
+        $r[] = d(strtoupper($str), ["style" => "background: #d7d7ff;padding: 5px;"]);
+        $r[] = monto_pago($monto);
+        $r[] = instruccion_pago($numero_cuenta);
+        return d(append($r), 6, 1);
 
 
-            $folio = prm_def($pago, "q2", "");
-            $monto = $pago["q"];
-            $concepto = "Saldo a cuenta Enid Service";
-
-            $r[] = d(get_form_saldos($beneficiario, $folio, $monto, $concepto, $numero_cuenta), 1);
+    }
 
 
-            $contendor_oxoo = btw(
-                img(
-                    [
-                        'src' => "http://enidservices.com/inicio/img_tema/portafolio/oxxo-logo.png",
-                        'style' => "width:100px!important;"
-                    ]
-                ),
-
-                d("ORDEN DE PAGO EN SUCURSALES OXXO"), "display_flex_enid"
-            );
+//    function get_form_monto_pago($info_pago)
+//    {
+//
+//        $r[] = h("MONTO A PAGAR");
+//        $r[] = h("$" . $info_pago["q"] . "MXN");
+//        $r[] = d("OXXO Cobrará una comisión adicional al momento de realizar el pago");
+//        return append($r);
+//    }
 
 
-            $r[] = d($contendor_oxoo, ["style" => "background: #02223e;color: white;"]);
-            $str_beneficiario = strtoupper(append([
-                    $concepto,
-                    "Beneficiario",
-                    $beneficiario,
-                    "Folio #",
-                    $folio
+    function form_saldos($beneficiario, $folio, $monto, $concepto, $numero_cuenta)
+    {
 
-                ])
-            );
+        $r[] = '<form action="../pdf/orden_pago.php" method="POST">';
+        $r[] = hiddens([
+            "name" => "beneficiario",
+            "value" => $beneficiario
 
-            $r[] = d($str_beneficiario, ["style" => "background: #d7d7ff;padding: 5px;"]);
-            $r[] = monto_pago($monto);
-            $r[] = get_instruccion_pago($numero_cuenta);
-            return d(append($r), 6, 1);
+        ]);
+        $r[] = hiddens([
+            "name" => "folio",
+            "value" => $folio
 
+        ]);
+        $r[] = hiddens([
+            "name" => "monto",
+            "value" => $monto
 
-        }
+        ]);
+        $r[] = hiddens([
+            "name" => "numero_cuenta",
+            "value" => $numero_cuenta
 
+        ]);
+        $r[] = hiddens([
+            "name" => "concepto",
+            "value" => $concepto
 
-        function get_form_monto_pago($info_pago)
-        {
-
-            $r[] = h("MONTO A PAGAR");
-            $r[] = h("$" . $info_pago["q"] . "MXN");
-            $r[] = d("OXXO Cobrará una comisión adicional al momento de realizar el pago");
-            return append($r);
-        }
-
-
-        function get_form_saldos($beneficiario, $folio, $monto, $concepto, $numero_cuenta)
-        {
-
-            $r[] = '<form action="../pdf/orden_pago.php" method="POST">';
-            $r[] = hiddens([
-                "name" => "beneficiario",
-                "value" => $beneficiario
-
-            ]);
-            $r[] = hiddens([
-                "name" => "folio",
-                "value" => $folio
-
-            ]);
-            $r[] = hiddens([
-                "name" => "monto",
-                "value" => $monto
-
-            ]);
-            $r[] = hiddens([
-                "name" => "numero_cuenta",
-                "value" => $numero_cuenta
-
-            ]);
-            $r[] = hiddens([
-                "name" => "concepto",
-                "value" => $concepto
-
-            ]);
-            $r[] = d(
-                btn(
-                    "IMPRIMIR",
-                    [
-                        "class" => " imprimir top_50"
-                    ]
-                    ,
-                    1
-                    ,
-                    1
-                ),
-
-                "col-lg-3 pull-right row"
-
+        ]);
+        $r[] = d(
+            btn(
+                "IMPRIMIR",
+                [
+                    "class" => " imprimir top_50"
+                ]
                 ,
                 1
-            );
+                ,
+                1
+            ),
 
-            $r[] = form_close();
-            return append($r);
+            "col-lg-3 pull-right row"
 
+            ,
+            1
+        );
 
-        }
-
-        function get_instrucciones()
-        {
-
-
-            $r [] = d(
-                "INSTRUCCIONES",
-                [
-                    "style" => "background: #000b39;color: white;padding: 5px;"
-                ],
-                1);
+        $r[] = form_close();
+        return append($r);
 
 
-            $r [] = d("1.-Acude a la tienda OXXO más cencana ");
-            $r [] = d("2.- Indica en caja que quieres realizar un
+    }
+
+    function instrucciones()
+    {
+
+
+        $r [] = d(
+            "INSTRUCCIONES",
+            [
+                "style" => "background: #000b39;color: white;padding: 5px;"
+            ],
+            1);
+
+
+        $r [] = d("1.-Acude a la tienda OXXO más cencana ");
+        $r [] = d("2.- Indica en caja que quieres realizar un
                                                 depósito en cuenta BBVA Bancomer ");
-            $r [] = d("3.- Proporciona el número de cuenta señalado");
-            $r [] = d("4.-Realiza el 
+        $r [] = d("3.- Proporciona el número de cuenta señalado");
+        $r [] = d("4.-Realiza el 
                         pago exacto correspondiente, que se indica en el monto a pagar");
-            $r [] = d("5.-Al confirmar tu pago, el cajero te entregará un comprobante impreso.");
-            $r [] = d("En el podrás verificar que se haya realizado correctamente, conserva este comprobante.");
+        $r [] = d("5.-Al confirmar tu pago, el cajero te entregará un comprobante impreso.");
+        $r [] = d("En el podrás verificar que se haya realizado correctamente, conserva este comprobante.");
 
-            $r [] = d("6.- Notifica tu pago desde tu área de cliente");
-            $r [] = a_enid("http://enidservices.com/inicio/login/",
-                [
-                    "href" => "http://enidservices.com/inicio/login/"
-                ]);
-            $r [] = d("ó", 1);
-            $r [] = d("Notifica tu pago  al área de ventas ventas@enidservices.com", 1);
-            $r [] = d(
-                img([
-                        "src" => path_enid("img_logo"),
-                    ]
-                ),
-                "col-lg-6 "
-                , 1
-            );
-
-            return d(append($r));
-
-
-        }
-
-        function get_instruccion_pago($numero_cuenta)
-        {
-
-
-            $r[] = img(
-                [
-                    "src" => "http://enidservices.com/inicio/img_tema/portafolio/logo-bbv.png",
-                    "style" => "width:300px!important;"
-
+        $r [] = d("6.- Notifica tu pago desde tu área de cliente");
+        $r [] = a_enid("http://enidservices.com/inicio/login/",
+            [
+                "href" => "http://enidservices.com/inicio/login/"
+            ]);
+        $r [] = d("ó", 1);
+        $r [] = d("Notifica tu pago  al área de ventas ventas@enidservices.com", 1);
+        $r [] = d(
+            img([
+                    "src" => path_enid("img_logo"),
                 ]
-            );
+            ),
+            "col-lg-6 "
+            , 1
+        );
 
-            $r[] = h($numero_cuenta, 4, ["style" => "color:blue;margin-bottom:30px;"]);
-            $r[] = get_instrucciones();
-            return addNRow(
-                d(
-                    append($r), "border padding_20 top_20"
-                )
-            );
+        return d(append($r));
 
-        }
 
-        function monto_pago($monto)
-        {
+    }
 
-            $r[] = h("MONTO A PAGAR");
-            $r[] = h("$" . $monto . "MXN", 2);
-            $r[] = d("OXXO Cobrará una comisión adicional al momento de realizar el pago", 1);
-            return d(append($r));
+    function instruccion_pago($numero_cuenta)
+    {
 
-        }
+
+        $r[] = img(
+            [
+                "src" => "http://enidservices.com/inicio/img_tema/portafolio/logo-bbv.png",
+                "style" => "width:300px!important;"
+
+            ]
+        );
+
+        $r[] = h($numero_cuenta, 4, ["style" => "color:blue;margin-bottom:30px;"]);
+        $r[] = instrucciones();
+        return addNRow(
+            d(
+                append($r), "border padding_20 top_20"
+            )
+        );
+
+    }
+
+    function monto_pago($monto)
+    {
+
+        $r[] = h("MONTO A PAGAR");
+        $r[] = h("$" . $monto . "MXN", 2);
+        $r[] = d("OXXO Cobrará una comisión adicional al momento de realizar el pago", 1);
+        return d(append($r));
+
+    }
 
 
 }
