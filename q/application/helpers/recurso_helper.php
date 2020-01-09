d5346ee2dfbfbd26b026af890a41553231d25942
arihgrey@gmail.com <arihgrey@gmail.com>
2020-01-09 15:00:24 +0100

    [FIX] se repara menu de perfiles y permisos

--- q/application/helpers/recurso_helper.php
+++ q/application/helpers/recurso_helper.php
@@ -5,11 +5,12 @@ if (!function_exists('invierte_date_time')) {
     function render_lista($data)
     {
         $b = 1;
+        $response = [];
         foreach ($data["recursos"] as $row) {
 
             $id_recurso = $row["id_recurso"];
             $tr = [];
-            $tr[] = td($b . ".-");
+            $tr[] = d(_text($b . ".-"));
 
             $conf = [
                 "type" => 'checkbox',
@@ -22,18 +23,20 @@ if (!function_exists('invierte_date_time')) {
 
             }
 
-            $tr[] = td(input($conf));
-            $tr[] = td($row["nombre"]);
-            $tr[] = td(a_enid("Ver sitio",
+            $tr[] = d(input($conf));
+            $tr[] = d(strip_tags($row["nombre"]),'text-uppercase ml-0 ml-md-2');
+            $tr[] = format_link("Ver sitio",
                 [
                     "href" => $row["urlpaginaweb"],
-                    "class" => 'blue_enid_background white ',
+                    "class" => 'blue_enid_background white ml-auto col-md-2',
                     "id" => $id_recurso,
                     "target" => '_black'
-                ]));
-            $l[] = tr(append($tr));
+                ]
+            );
+
+            $response[] = d($tr,'d-flex mt-3');
             $b++;
         }
-        return tb(append($l));
+        return append($response);
     }
 }
