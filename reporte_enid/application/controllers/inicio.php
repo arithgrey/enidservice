5a56c74b6618ab46084419c823e09b80f19b094a
arihgrey@gmail.com <arihgrey@gmail.com>
2020-04-07 09:10:22 +0200

    [FEAT] se simplifica la busqueda de artículos en el proceso

--- reporte_enid/application/controllers/inicio.php
+++ reporte_enid/application/controllers/inicio.php
@@ -42,7 +42,7 @@ class Inicio extends CI_Controller
                 break;
 
             case 6:
-                $module = "location:../pedidos";
+                $module = "location:../busqueda";
                 break;
 
 
