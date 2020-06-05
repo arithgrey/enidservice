4437a29614151a878b47132b296c2c4ff7c12e72
arihgrey@gmail.com <arihgrey@gmail.com>
2020-06-05 08:06:46 +0200

    [CHORE]

--- entregas/application/helpers/entregas_helper.php
+++ entregas/application/helpers/entregas_helper.php
@@ -15,28 +15,40 @@ if (!function_exists('invierte_date_time')) {
 
     }
 
-    function calendario($data)
+
+    function text_tiempo_entrega($dias, $es_mayor, $ubicacion)
     {
-        $fecha = horario_enid();
-        $proximas_entregas = $data['proximas_entregas'];
-        $hoy = $fecha->format('Y-m-d');
-        $franja_horaria = [];
-
-        for ($a = 0; $a < 6; $a++) {
-
-            $fecha = add_date($hoy, $a);
-            $franja = [];
-            $franja[] = franja_horaria($fecha, $proximas_entregas);
-            $franja_horaria[] = append($franja);
-
+        $response = '';
+        if ($dias == 1 && $es_mayor) {
+            $response = 'Se entregará mañana';
+        } elseif ($dias == 1 && !$es_mayor) {
+            $response = 'La entrega fué ayer';
+        } elseif ($ubicacion > 0) {
+            $response = '';
         }
-
-        $response[] = d(_titulo('Próximas entregas', 2), 12);
-        $response[] = d($franja_horaria, 12);
-        return append($response);
-
+        return $response;
     }
 
+    function formato_hora($es_contra_entrega, $ubicacion, $row)
+    {
+        $response = 0;
+        $es_contra_entrega_domicilio_sin_direccion = false;
+        if ($es_contra_entrega) {
+            $es_contra_entrega_domicilio_sin_direccion = $row['es_contra_entrega_domicilio_sin_direccion'];
+            $response = 1;
+            if ($es_contra_entrega_domicilio_sin_direccion) {
+                $response = 0;
+            }
+            if ($ubicacion > 0) {
+                $response = 1;
+            }
+        }
+        return [
+            'formato_hora' => $response,
+            'es_contra_entrega_domicilio_sin_direccion' => $es_contra_entrega_domicilio_sin_direccion
+        ];
+
+    }
 
     function franja_entregas($recibos, $data)
     {
@@ -44,7 +56,6 @@ if (!function_exists('invierte_date_time')) {
         $r = [];
         $es_reparto = 1;
         $f = 0;
-        $ventas_posteriores = [];
         $ventas_hoy = [];
         if (es_data($recibos)) {
 
@@ -57,80 +68,72 @@ if (!function_exists('invierte_date_time')) {
 
                 $hoy = $fecha->format('Y-m-d');
                 $es_mayor = ($fecha_entrega > $hoy);
-
                 $dias = date_difference($hoy, $fecha_entrega);
-
                 $es_menor = ($dias > 0 && !$es_mayor);
-
-                $text_entrega = _text_('Se entregará en ', $dias, 'días!');
-                $text_entrega_paso = _text_('Pendiente desde hace', $dias, 'días');
-                $text_entrega = (!$es_mayor) ? $text_entrega_paso : $text_entrega;
                 $id_recibo = $row['id_recibo'];
 
                 $ubicacion = $row['ubicacion'];
-                if ($dias == 1 && $es_mayor) {
-                    $text_entrega = 'Se entregará mañana';
-                } elseif ($dias == 1 && !$es_mayor) {
-                    $text_entrega = 'La entrega fué ayer';
-                } elseif ($ubicacion > 0) {
-                    $text_entrega = '';
-                }
-
-
+                $text_entrega = text_tiempo_entrega($dias, $es_mayor, $ubicacion);
                 $tipo_entrega = $row['tipo_entrega'];
                 $es_contra_entrega = $row['es_contra_entrega'];
-                $es_contra_entrega_domicilio_sin_direccion = false;
-                $format_hora = 0;
-                if ($es_contra_entrega) {
-                    $es_contra_entrega_domicilio_sin_direccion = $row['es_contra_entrega_domicilio_sin_direccion'];
-                    $format_hora = 1;
-                    if ($es_contra_entrega_domicilio_sin_direccion) {
-                        $format_hora = 0;
-                    }
-                    if ($ubicacion > 0) {
-                        $format_hora = 1;
-                    }
-                }
 
-                $es_formato_hora = (($tipo_entrega == 1) || ($tipo_entrega == 2 && $format_hora));
+                $format_hora = formato_hora($es_contra_entrega, $ubicacion, $row);
+                $es_contra_entrega_domicilio_sin_direccion = $format_hora['es_contra_entrega_domicilio_sin_direccion'];
+                $es_formato_hora = (($tipo_entrega == 1) || ($tipo_entrega == 2 && $format_hora['formato_hora']));
                 $hora_entrega = ($es_formato_hora) ? format_hora($fecha_contra_entrega) : '';
                 $notificacion_hoy = ($hoy === $fecha_entrega) ? '' : $text_entrega;
 
                 $es_hoy = ($hoy === $fecha_entrega) ? 'bg-dark white' : '';
                 $dia_entrega = d(_text_($notificacion_hoy, $hora_entrega), _text_('strong', $es_hoy));
 
-                $imagenes = d(img($row["url_img_servicio"]), 'w-25 mx-auto mt-5');
+                $imagenes = d(img($row["url_img_servicio"]), 'w-50 mx-auto');
                 $totales_recibo = money($row["total"]);
                 $total = d($totales_recibo, "black");
                 $id_usuario_entrega = $row['id_usuario_entrega'];
-
-
                 $ubicacion = $row['ubicacion'];
 
-                $text_total = nota_pedido(
+
+                $nota_pedido = nota_pedido(
                     $total,
                     $dia_entrega,
                     $es_contra_entrega,
                     $es_contra_entrega_domicilio_sin_direccion,
-                    $id_usuario_entrega,
                     $ubicacion,
                     $text_entrega,
-                    $row
+                    $row, $id_recibo
                 );
 
+                $abrir_en_google = $nota_pedido['abrir_en_google'];
+                $maps_link = $nota_pedido['maps_link'];
+                $descripcion_domicilio = $nota_pedido['descripcion_domicilio'];
 
                 $orden = d(_text('#', $id_recibo), 'text-center');
 
-                $text = _d($imagenes, $orden, $text_total);
+                $icon = d(icon(_text_(_entregas_icon, 'fa-2x')), 'black text-right');
+                $indicador_entrega = ($id_usuario_entrega > 0) ? $icon : '';
+                $filtro = ($id_usuario_entrega > 0) ? 'reparto_asignado' : '';
+
+                $text = _d($indicador_entrega, $imagenes, $orden, $descripcion_domicilio);
 
                 $desglose_pedido = path_enid("pedidos_recibo", $row["id_recibo"]);
                 $tracker = path_enid("pedido_seguimiento", $row["id_recibo"]);
                 $url = ($es_reparto > 0) ? $tracker : $desglose_pedido;
+                $link = a_enid($text,
+                    [
+                        'href' => $url,
+                        'class' => 'col-md-4 col-md-offset-4'
+                    ]
+                );
+                $pedido = [];
+                $pedido[] = d($link, "row");
 
+                if ($abrir_en_google > 0) {
+                    $seccion_maps = d($maps_link, 'col-md-4 col-md-offset-4');
+                    $pedido[] = d($seccion_maps, "row mt-1 mb-5 ");
+                }
 
-                $linea = d(a_enid($text, $url), "border-bottom");
                 if ($es_hoy || $es_menor) {
-                    $ventas_hoy[] = $linea;
+                    $ventas_hoy[] = d($pedido,$filtro);
                 }
 
                 $f++;
@@ -138,8 +141,11 @@ if (!function_exists('invierte_date_time')) {
 
             if (es_data($recibos)) {
 
-                $titulo = 'Entregas en proceso';
-                $r[] = d(_titulo($titulo));
+                $r[] = d(_titulo('Entregas en proceso'), 'mb-5');
+                $r[] = d(icon(_text_(_mas_opciones_icon,'fa-2x filtro_menos_opciones')   ),
+                    ['class' => 'mx-auto mb-5'], 13);
+                $r[] = d(icon(_text_(_mas_opciones_bajo_icon,'fa-2x filtro_mas_opciones d-none')   ),
+                    ['class' => 'mx-auto mb-5'], 13);
                 $r[] = append($ventas_hoy);
 
             }
@@ -151,27 +157,24 @@ if (!function_exists('invierte_date_time')) {
 
     function nota_pedido($total, $dia_entrega, $es_contra_entrega,
                          $es_contra_entrega_domicilio_sin_direccion,
-                         $id_usuario_entrega, $ubicacion, $text_entrega, $row)
+                         $ubicacion, $text_entrega, $row, $id_recibo)
     {
-        $text_total = [];
-        $text_total[] = d($total);
-        $text_total[] = $dia_entrega;
-
+        $response = [];
+        $response[] = d($total);
+        $response[] = $dia_entrega;
         $tipo_entrega = $row['tipo_entrega'];
+        $abrir_en_google = 0;
+        $maps_link = '';
 
+        if ($id_recibo == 1419) {
+
+            $a = 0;
+        }
         switch ($tipo_entrega) {
             case 1:
 
                 $id_punto_encuentro = $row['id_punto_encuentro'];
-                if ($id_punto_encuentro > 0) {
-                    $punto_encuentro = $row['data_punto_encuentro'];
-                    if (es_data($punto_encuentro)) {
-                        $nombre = $punto_encuentro["nombre"];
-                        $text_total[] = d(_text_('Estación del metro', $nombre), 'mb-5 text-uppercase strong black text-center');
-                    }
-                } else {
-                    $text_total[] = d('(Pedir ubicación)', 'text-danger mb-5');
-                }
+                $response[] = descripcion_entregas_puntos_encuentro($id_punto_encuentro, $row);
 
                 break;
 
@@ -179,19 +182,19 @@ if (!function_exists('invierte_date_time')) {
 
                 $id_domicilio = $row['id_domicilio'];
                 $data_domicilio = $row['data_domicilio'];
+                $descripcion_domicilio = descripcion_entregas_domicilio(
+                    $row,
+                    $id_domicilio,
+                    $data_domicilio,
+                    $es_contra_entrega,
+                    $ubicacion,
+                    $text_entrega,
+                    $es_contra_entrega_domicilio_sin_direccion
+                );
+                $abrir_en_google = $descripcion_domicilio['abrir_en_google'];
+                $maps_link = $descripcion_domicilio['maps_link'];
+                $response[] = $descripcion_domicilio['texto_direccion'];
 
-                if ($es_contra_entrega) {
-                    $text_total[] = ($ubicacion < 1) ? '' : d($text_entrega);
-                    if ($es_contra_entrega_domicilio_sin_direccion && $ubicacion < 1) {
-                        $text_total[] = d('(Pedir ubicación)', 'text-danger mb-5');
-                    } else if ($ubicacion > 0) {
-                        $text_ubicacion = text_ubicacion($row['data_ubicacion']);
-                        $text_total[] = d($text_ubicacion, 'mb-5 black text-center');
-                    } else if ($id_domicilio > 0) {
-                        $text_domicilio = text_domicilio($data_domicilio);
-                        $text_total[] = d($text_domicilio, 'mb-5 black text-center');
-                    }
-                }
                 break;
 
             default:
@@ -199,10 +202,70 @@ if (!function_exists('invierte_date_time')) {
         }
 
 
-        return $text_total;
+        return
+            [
+                'descripcion_domicilio' => $response,
+                'abrir_en_google' => $abrir_en_google,
+                'maps_link' => $maps_link
+            ];
+
+    }
+
+    function descripcion_entregas_puntos_encuentro($id_punto_encuentro, $row)
+    {
+        $response = '';
+        if ($id_punto_encuentro > 0) {
+            $punto_encuentro = $row['data_punto_encuentro'];
+            if (es_data($punto_encuentro)) {
+                $nombre = $punto_encuentro["nombre"];
+                $response = d(_text_('Estación del metro', $nombre),
+                    'mb-5 text-uppercase strong black text-center'
+                );
+            }
+        } else {
+            $response = d('(Pedir ubicación)', 'text-danger mb-5');
+        }
+        return $response;
+    }
+
+    function descripcion_entregas_domicilio($row, $id_domicilio, $data_domicilio,
+                                            $es_contra_entrega, $ubicacion, $text_entrega,
+                                            $es_contra_entrega_domicilio_sin_direccion)
+    {
+        $abrir_en_google = 0;
+        $response = '';
+        $maps_link = '';
+        $solicitar_ubicacion = 0;
+        if ($es_contra_entrega) {
+            $response = ($ubicacion < 1) ? '' : d($text_entrega);
+            if ($es_contra_entrega_domicilio_sin_direccion && $ubicacion < 1) {
+                $response = d('(Pedir ubicación)', 'text-danger mb-5');
+                $solicitar_ubicacion++;
+
+            } else if ($ubicacion > 0) {
+
+                $text_ubicacion = prm_def($row['data_ubicacion'], 'ubicacion');
+                $response = d($text_ubicacion, 'mb-5 black text-center');
+                $maps_link = texto_maps($text_ubicacion, 0);
+                $abrir_en_google++;
+
+            } else if ($id_domicilio > 0) {
+
+                $text_domicilio = text_domicilio($data_domicilio);
+                $response = d($text_domicilio, 'mb-5 black text-center');
+            }
+        }
+        return
+            [
+                'texto_direccion' => $response,
+                'abrir_en_google' => $abrir_en_google,
+                'maps_link' => $maps_link,
+                'solicitar_ubicacion' => $solicitar_ubicacion
+            ];
 
 
     }
+
 
     function text_domicilio($domicilio)
     {
@@ -240,90 +303,24 @@ if (!function_exists('invierte_date_time')) {
         return $response;
     }
 
-    function text_ubicacion($domicilio)
+    function texto_maps($domicilio)
     {
-        $response = prm_def($domicilio, 'ubicacion');
-        $response = valida_texto_maps($response, 0);
+        $ubicacion_arreglo = explode(' ', $domicilio);
+        $text = '';
+        foreach ($ubicacion_arreglo as $row) {
+            if (strpos($row, 'https') !== FALSE) {
 
-        return $response;
-    }
+                $conf = [
+                    'href' => $row,
+                    'target' => '_blank',
+                    'class' => 'text-uppercase black mt-3'
+                ];
 
-    function franja_horaria($fecha, $proximas_entregas)
-    {
+                $text = format_link('abrir en google maps', $conf);
 
-
-        $horarios = array_horarios();
-        $horario_enid = horario_enid();
-        $hora_actual = $horario_enid->format('H');
-        $hoy = $horario_enid->format('Y-m-d');
-        $es_hoy = ($hoy == $fecha);
-
-
-        $str_fecha = ($es_hoy) ? 'Hoy ' : format_fecha($fecha);
-        $fecha_titulo = d($str_fecha, _strong);
-        $fechas_contra_entrega = (es_data($proximas_entregas)) ? array_column($proximas_entregas, 'fecha_contra_entrega') : [];
-        $hay_ordenes_ocultas = 0;
-        $total_dia = 0;
-        foreach ($horarios as $row) {
-
-            $total_pedidos_franja_horaria = busqueda_horario($row, $fecha, $fechas_contra_entrega);
-            $extra = ($total_pedidos_franja_horaria > 0) ? 'text-white bg-primary strong f11 entregas_franja_horaria cursor_pointer' : '';
-            if ($total_pedidos_franja_horaria > 0) {
-
-                $total_dia = ($total_dia + $total_pedidos_franja_horaria);
-            }
-
-            $text_entregas_franja_horaria = ($total_pedidos_franja_horaria > 0) ? $total_pedidos_franja_horaria : '';
-
-            $franja = _text($fecha, ' ', $row);
-            $horario = date_create($row)->format('H');
-            $extra_franja_horaria = ($es_hoy && $hora_actual > $horario) ? 'opciones_ocultas d-none' : '';
-            if ($es_hoy && $hora_actual > $horario) {
-                $hay_ordenes_ocultas++;
-            }
-            $config = [
-                'class' => _text_('border-bottom mt-5', $extra, $extra_franja_horaria)
-            ];
-            if (($total_pedidos_franja_horaria > 0)) {
-                $config['onclick'] = 'busqueda_ordenes_franja_horaria("' . $franja . '")';
-            }
-            $response[] = d(
-                _d($row, $text_entregas_franja_horaria), $config
-            );
-
-        }
-
-        $str_total = ($total_dia > 0) ? $total_dia : '';
-        $entregas_programadas = d($str_total);
-        $text = ($total_dia > 0) ? 'Entregas programadas' : 'Sin entregas';
-        $bottom = ($total_dia > 0) ? '' : 'mb-4';
-        $str_entregas = d($text, _text_('fp8 text-uppercase', $bottom));
-        $totales = d(_text_($str_entregas, $entregas_programadas), 'border-bottom');
-        if ($hay_ordenes_ocultas > 0) {
-            array_unshift($response, icon(_text_(_mas_opciones_bajo_icon, 'mostrar_todo')));
-        }
-        array_unshift($response, $totales);
-        array_unshift($response, $fecha_titulo);
-
-        return d($response, 'border col-md-2 text-center border-secondary p-0');
-    }
-
-    function busqueda_horario($horario, $fecha_calendario, $lista_horarios)
-    {
-
-        $pedidos_en_horario = 0;
-        foreach ($lista_horarios as $row) {
-            $fecha_entrega = date_create($row)->format('Y-m-d');
-            if ($fecha_entrega == $fecha_calendario) {
-                $hora_entrega = date_create($row)->format('H:i');
-                if ($hora_entrega == $horario) {
-
-                    $pedidos_en_horario++;
-                }
             }
         }
-        return $pedidos_en_horario;
+        return $text;
     }
-
 
 }
