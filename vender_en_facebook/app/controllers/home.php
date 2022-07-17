d53ea5d83180ef926ae1c2d099efc4646bd2f2d5
arihgrey@gmail.com <arihgrey@gmail.com>
2022-07-18 00:14:29 +0200

    [FEAT] como vender en marketplace de Facebook y whatsApp business

---
+++ vender_en_facebook/app/controllers/home.php
@@ -0,0 +1,23 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+    private $data;
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->helper("pago");
+        $this->load->library(lib_def());
+        $this->data = $this->app->session();
+
+    }
+
+    function index()
+    {
+
+        $data = $this->app->cssJs($this->data, "forma_pago");
+        $this->app->pagina($data , get_format_pago(), 1);
+
+    }
+    
+}
