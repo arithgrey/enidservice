16b25ff1bd3fea64004b40ef29985a08b59639da
arihgrey@gmail.com <arihgrey@gmail.com>
2022-09-27 00:44:52 -0500

    [chore]

--- def.php
+++ def.php
@@ -1,6 +1,8 @@
 <?php
 header("Cache-Control: no-cache, must-revalidate");
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+date_default_timezone_set("America/Mexico_City");
+
 define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 switch (ENVIRONMENT) {
 	case 'development':		
