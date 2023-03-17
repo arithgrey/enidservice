3489968da0bbd9a2e3ae308ea8c7d77753fc6ee6
arihgrey@gmail.com <arihgrey@gmail.com>
2023-03-17 11:07:05 -0600

    [chore]

--- login/app/controllers/api/sess.php
+++ login/app/controllers/api/sess.php
@@ -14,42 +14,42 @@ class Sess extends REST_Controller
 
         $param = $this->post();
         $response = false;
-        $es_ajax = $this->input->is_ajax_request();
-        $es_barer = (array_key_exists("t", $param) && $param["t"] == $this->config->item('barer'));
+
+        $es_barer = (prm_def($param,"t") === $this->config->item('barer'));
+
         
-        if ($es_ajax || $es_barer) {
-            $response = [];
-            if (fx($param, "email,secret")) {
-                $usuario = $this->get_es_usuario($param);
-                $response["usuario"] = $usuario;
-                $response["login"] = false;
-                if (es_data($usuario)) {
+        $response = [];
+        if (fx($param, "email,secret")) {
+            $usuario = $this->get_es_usuario($param);
+            $response["usuario"] = $usuario;
+            $response["login"] = false;
+            if (es_data($usuario)) {
 
-                    $usuario = $usuario[0];
-                    $id_usuario = $usuario["id"];
-                    $nombre = $usuario["name"];
-                    $email = $usuario["email"];
-                    $id_empresa = $usuario["id_empresa"];
+                $usuario = $usuario[0];
+                $id_usuario = $usuario["id"];
+                $nombre = $usuario["name"];
+                $email = $usuario["email"];
+                $id_empresa = $usuario["id_empresa"];
 
-                    $recien_creado = ($es_barer);
-                    $session = $this->app->crea_session(
-                            $id_usuario, 
-                            $nombre, 
-                            $email, 
-                            $id_empresa, 
-                            $recien_creado);
+                $recien_creado = ($es_barer);
+                $session = $this->app->crea_session(
+                    $id_usuario,
+                    $nombre,
+                    $email,
+                    $id_empresa,
+                    $recien_creado
+                );
 
-                    $response["session"] = $session;
-                    $response["session_creada"] = $this->app->get_session();
+                $response["session"] = $session;
+                $response["session_creada"] = $this->app->get_session();
 
-                
-                    if ($es_barer) {
 
-                        $this->response($session);
-                    }
-                    
-                    $response["login"] = (es_data($session)) ? path_enid("login") : false;
+                if ($es_barer) {
+
+                    $this->response($session);
                 }
+
+                $response["login"] = (es_data($session)) ? path_enid("login") : false;
             }
         }
         $this->response($response);
