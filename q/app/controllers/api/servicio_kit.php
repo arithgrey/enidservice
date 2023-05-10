b6431827ed460f9e52fda4f909311f973525d99a
arihgrey@gmail.com <arihgrey@gmail.com>
2023-05-10 02:46:03 -0600

    [FEAT] ahora se pueden asignar a un alamacen kits

---
+++ q/app/controllers/api/servicio_kit.php
@@ -0,0 +1,90 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH . '../../librerias/REST_Controller.php';
+
+class servicio_kit extends REST_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model("servicio_kit_model");
+		$this->load->library(lib_def());
+	}	
+    function servicios_por_kit_GET()
+	{
+        $param = $this->get();
+        $response = false;
+        if (fx($param, "id_kit")) {
+
+            $response = $this->servicio_kit_model->get([], ["id_kit" => $param["id_kit"]],100);
+
+        }
+        $this->response($response);
+
+	}
+
+
+    /*
+    function index_GET()
+	{
+        $param = $this->get();
+        $response = false;
+        if (fx($param, "id")) {
+q
+            $response = $this->servicio_kit_model->get([], ["id" => $param["id"]],100);
+
+        }
+        $this->response($response);
+
+	}
+
+    function index_PUT()
+	{
+		$param = $this->put();
+		$response = false;
+		
+		if (fx($param, "")) {
+            
+            $id = $param["id"];
+			$params = [
+				"" => $param[""],
+				"" => $param[""]
+			];
+
+            
+			$response = $this->servicio_kit_model->update($params,  ["id" => $id]);
+
+		}
+		$this->response($response);
+	}
+
+    function index_DELETE()
+	{
+		$param = $this->delete();
+		$response = false;
+		
+		if (fx($param, "id")) {
+            
+            $id = $param["id"];
+			        
+			$response = $this->servicio_kit_model->delete(["id" => $id]);
+
+		}
+		$this->response($response);
+	}
+    function id_GET()
+    {
+        $param = $this->get();
+		$response = false;
+		
+
+        if (fx($param, "id")) {
+            
+            $id = $param["id"];
+            $response = $this->response($this->servicio_kit_model->q_get($id));
+        }
+        $this->response($response);
+    }
+    */
+    
+
+}
