b50d4cb3aae8c9ba72b55a6b4c95d0806f3693ea
arihgrey@gmail.com <arihgrey@gmail.com>
2023-05-18 02:04:57 -0600

    [FEAT] como administrador quiero poder agregar kits base con la finalidad de poder agilizar la gestión de mi inventario

--- kits/app/helpers/kit_helper.php
+++ kits/app/helpers/kit_helper.php
@@ -5,25 +5,50 @@ if (!function_exists('invierte_date_time')) {
     function render($data)
     {
 
-        $response[] = d($data["kits"],13);
-        $response[]  = d(modal_kits_servicio(),13);
-        return d($response,10,1);
+        $seccion_kits[] = d($data["kits"], 13);
+        $seccion_kits[]  = d(modal_kits_servicio(), 13);
+        $seccion_kits[] = d(modal_nuevo_kit(), 13);
+
+        $response[] = d(format_link("+ Nuevo kit", ["class" => "boton_nuevo_kit"]), 2);
+        $response[] = d($seccion_kits, 10);
+
+        return d($response, 10, 1);
     }
-    function modal_kits_servicio(){
-        
-        $contenido[] = input_frm(12, 'Filtrar',
-        [
-            "id" => "textinput",
-            "name" => "textinput",
-            "placeholder" => "Nombre del producto o servicio",
-            "class" => "q_recompensa",
-            "onkeyup" => "onkeyup_colfield_check(event);"
-        ]
+    function modal_kits_servicio()
+    {
+
+        $contenido[] = input_frm(
+            12,
+            'Filtrar',
+            [
+                "id" => "textinput",
+                "name" => "textinput",
+                "placeholder" => "Nombre del producto o servicio",
+                "class" => "q_recompensa",
+                "onkeyup" => "onkeyup_colfield_check(event);"
+            ]
         );
 
-        $contenido[] = place("place_nuevo_servicio_kit");                  
+        $contenido[] = place("place_nuevo_servicio_kit");
         return gb_modal($contenido, 'modal_servicios_kit');
     }
+    function modal_nuevo_kit()
+    {
 
-    
+        $contenido[] = form_open("",["class" => "form_kit"]);
+        $contenido[] = input_frm(
+            12,
+            'Nombre del kit',
+            [
+                "id" => "text_input_kit",
+                "name" => "nombre",
+                "placeholder" => "Nombre del kit",
+                "class" => "nombre_kit",
+                "onkeyup" => "onkeyup_colfield_check(event);"
+            ]
+        );        
+        $contenido[] = form_close();
+
+        return gb_modal($contenido, 'modal_kit');
+    }
 }
